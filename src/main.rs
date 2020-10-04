649eb351bd2c97cd14c9daf175da8215b2472730
Tommy Malmqvist <tommy.malmqvist@codelabs.se>
2020-10-04 16:04:18 +0200

    Add Limit struct

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,20 @@ struct Order {
     parrent_limit: Box<Option<Limit>>,
 }
 
-struct Limit {}
+struct Limit {
+    price: f64,
+    size: u32,   // is this needed?
+    volume: u32, // is this needed?
+    parrent: Box<Option<Limit>>,
+    left_child: Box<Option<Limit>>,
+    right_child: Box<Option<Limit>>,
+    head_order: Box<Option<Order>>,
+    tail_order: Box<Option<Order>>,
+}
+
+struct Book {
+    // not implemented
+}
 
 fn main() {
     println!("Hello, world!");
